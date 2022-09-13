3e146741d99059e6221e1125ddcf7975c4eb5e88
AdamsAdinatu <adinatu23@gmail.com>
2022-09-13 14:35:46 +0000

    login authorization

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,25 +2,45 @@ import { createContext, useState } from 'react';
 import React from 'react';
 
 const AuthContext = createContext();
-AuthContext = AuthContext.Provider;
+ const AuthProvider = AuthContext.Provider;
 
 function AuthContextProvider() {
 
     const [success, setsuccess] =useState("false");
     const [error, setError] =useState("null");
     const [loading, setLoading] =useState("false");
-    const [userr, setUser] =useState("null");
+    const [user, setUser] =useState("null");
 
 
     //signup function
     const authRegister= async (username, email, password)=>{
-     await fetch(URL,{
-      method:"POST"
+      setLoading(true);
+     await fetch("/api/users/register",{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
      })
-    }
-  return (
+     .them((res) => res.json())
+     .them((data) => {
+      console.log(data);
+      setsuccess(true);
+      setUser(data);
+     })
+     .catch((err) => {
+      console.log(err);
+      setError(err);
+     });
+     setLoading(false);
+
+    };
+  return(
     <div>
-      
     </div>
   )
 }
